adc9b9fdcf102b8dac053b39f384110446c26c22
Rafael Santos Pereira <rsantope@everis.com>
2019-12-13 14:23:50 -0300

    Eva

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RestController
+	class HelloController {
+		@RequestMapping("/hello")
+		@GetMapping
+		String hello() {
+			return "Hello World " + " !";
+
+		}
+	}
+}
